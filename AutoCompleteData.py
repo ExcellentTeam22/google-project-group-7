e975ddf73d10d1e85dd89d505abd8e91c67b6726
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-07-25 12:06:23 +0300

    class ready

---
+++ AutoCompleteData.py
@@ -0,0 +1,15 @@
+class AutoCompleteData:
+
+
+  def  __init__(self, completed_sentence, source_text,offset, score ):
+
+      self.completed_sentence= completed_sentence
+      self.source_text  = source_text
+      self.offset= offset
+      self.score = score
+
+
+  def my_function(self):
+     print (self.completed_sentence, self.source_text, self.offset, self.score)
+
+
